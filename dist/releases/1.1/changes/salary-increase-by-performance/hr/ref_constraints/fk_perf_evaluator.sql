4e9f3ecfb8e0c1c90e8ba75eb01e5664a868714b
Oracle Public Cloud User <opc@democlient.subnet08111310.vcn08111310.oraclevcn.com>
2025-11-16 18:12:12 +0000

    feat: dist for pay rise for employees by performance but only those without revision for two years

---
+++ dist/releases/1.1/changes/salary-increase-by-performance/hr/ref_constraints/fk_perf_evaluator.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+-- changeset HR:1763316730547 stripComments:false  logicalFilePath:salary-increase-by-performance/hr/ref_constraints/fk_perf_evaluator.sql
+-- sqlcl_snapshot src/database/hr/ref_constraints/fk_perf_evaluator.sql:null:6da2060db1188c1c630b66be1eed4313af6b8936:create
+
+alter table hr.employee_performance
+    add constraint fk_perf_evaluator
+        foreign key ( evaluator_id )
+            references hr.employees ( employee_id )
+        enable;
+
