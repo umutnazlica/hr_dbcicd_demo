9b518571d5bcd064664cf0735706686110ccf7e2
Oracle Public Cloud User <opc@democlient.subnet08111310.vcn08111310.oraclevcn.com>
2025-11-17 06:29:28 +0000

    feat: dist for pay rise for employees by performance but only those without revision for two years

---
+++ dist/releases/1.1/changes/salary-increase-by-performance/hr/procedures/salary_increase.sql
@@ -0,0 +1,50 @@
+-- liquibase formatted sql
+-- changeset HR:1763360965752 stripComments:false  logicalFilePath:salary-increase-by-performance/hr/procedures/salary_increase.sql
+-- sqlcl_snapshot src/database/hr/procedures/salary_increase.sql:null:59e4c88f76d4d214c2a5b93b41c5727d4408ec47:create
+
+create or replace procedure hr.salary_increase is
+begin
+    update employees e
+    set
+        salary = salary * (
+            select
+                case
+                    when ep.rating = 5 then
+                        1.10
+                    when ep.rating = 4 then
+                        1.05
+                    else
+                        1
+                end
+            from
+                employee_performance ep
+            where
+                    ep.employee_id = e.employee_id
+                and ep.perf_date = (
+                    select
+                        max(ep2.perf_date)
+                    from
+                        employee_performance ep2
+                    where
+                        ep2.employee_id = e.employee_id
+                )
+        ),
+        last_salary_review = sysdate
+    where
+        ( last_salary_review is null
+          or last_salary_review < add_months(sysdate, -24) )
+        and fire_date is null
+        and exists (
+            select
+                1
+            from
+                employee_performance ep
+            where
+                    ep.employee_id = e.employee_id
+                and ep.rating >= 4
+                and ep.perf_date >= add_months(sysdate, -12)
+        );
+
+end;
+/
+
---
+++ dist/releases/1.1/changes/salary-increase-by-performance/hr/ref_constraints/fk_perf_employee.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+-- changeset HR:1763360965956 stripComments:false  logicalFilePath:salary-increase-by-performance/hr/ref_constraints/fk_perf_employee.sql
+-- sqlcl_snapshot src/database/hr/ref_constraints/fk_perf_employee.sql:null:0ae995ccc97d2be638ce6c50cfc3ffa92aaaa5e1:create
+
+alter table hr.employee_performance
+    add constraint fk_perf_employee
+        foreign key ( employee_id )
+            references hr.employees ( employee_id )
+        enable;
+
